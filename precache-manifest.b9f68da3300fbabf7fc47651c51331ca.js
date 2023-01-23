d193c0269be7e3f655c150ecfcfe575ec51a936a
Michael Kness <mpkness@gmail.com>
2023-01-22 17:47:46 -0800

    Updates

---
+++ precache-manifest.b9f68da3300fbabf7fc47651c51331ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8b5c13b1a27c2ae92b",
+    "url": "/asterisk-lounge-bgg/static/css/main.eff22b30.chunk.css"
+  },
+  {
+    "revision": "8c8b5c13b1a27c2ae92b",
+    "url": "/asterisk-lounge-bgg/static/js/main.b358d483.chunk.js"
+  },
+  {
+    "revision": "6006364049e8a662cdab",
+    "url": "/asterisk-lounge-bgg/static/js/runtime~main.1119b971.js"
+  },
+  {
+    "revision": "cbe152389f6cc4538563",
+    "url": "/asterisk-lounge-bgg/static/js/2.c2ed99c8.chunk.js"
+  },
+  {
+    "revision": "551e95b27638874fde2a",
+    "url": "/asterisk-lounge-bgg/static/js/3.ff39eaff.chunk.js"
+  },
+  {
+    "revision": "bb03f488f0469129d1486c67311f195a",
+    "url": "/asterisk-lounge-bgg/index.html"
+  }
+];
